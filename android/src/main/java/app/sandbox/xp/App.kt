49c049a50c3f25eb2d30e78d95579622cc8d3abe
Rafael Toledo <rafaeldtoledo@gmail.com>
2020-09-22 23:16:06 -0300

    Include Firebase Remote Config dependencies

--- android/src/main/java/app/sandbox/xp/App.kt
+++ android/src/main/java/app/sandbox/xp/App.kt
@@ -1,6 +1,26 @@
 package app.sandbox.xp
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 
 class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Firebase.remoteConfig.apply {
+            setDefaultsAsync(R.xml.remote_config_defaults)
+            fetchAndActivate().addOnCompleteListener {
+                Log.d(
+                    "FETCHING_EXPERIMENTS", if (it.isSuccessful) {
+                        "Completed with success"
+                    } else {
+                        "Failed to fetch"
+                    }
+                )
+            }
+        }
+    }
 }
